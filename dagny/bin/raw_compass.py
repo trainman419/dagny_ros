c08ae98a1afff9ef3bf3ae9c4131c589a8f0fa29
trainman419 <legotown@aol.com>
2014-06-15 17:01:20 -0700

    Add script for re-interpreting raw compass values

---
+++ dagny/bin/raw_compass.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+from math import atan2
+
+import rospy
+import tf
+
+# messages
+from geometry_msgs.msg import Vector3Stamped, Quaternion
+from sensor_msgs.msg import Imu
+
+# observed min and max values
+x_min =  10.0
+x_max = -10.0
+y_min =  10.0
+y_max = -10.0
+
+x_center = 0.1
+x_scale = 1.0
+
+y_center = -0.1
+y_scale = 1.0
+
+autocompute = False
+
+imu_pub = None
+
+def magnetic_cb(msg):
+    global x_max, x_min, y_max, y_min, x_center, y_center, x_scale, y_scale
+    if autocompute:
+        x_max = max(x_max, msg.vector.x)
+        x_min = min(x_min, msg.vector.x)
+
+        y_max = max(y_max, msg.vector.y)
+        y_min = min(y_min, msg.vector.y)
+
+        x_center = (x_max + x_min) / 2.0
+        y_center = (y_max + y_min) / 2.0
+
+        x_scale = x_max - x_min
+        y_scale = y_max - y_min
+
+    x = (msg.vector.x - x_center) / x_scale
+    y = (msg.vector.y - y_center) / y_scale
+
+    heading = atan2(x, y)
+
+    imu = Imu()
+    imu.header = msg.header
+    imu.header.frame_id = 'base_link'
+    q = tf.transformations.quaternion_from_euler(0, 0, heading)
+    imu.orientation = Quaternion(*q)
+
+    imu.orientation_covariance = [ 0.1, 0.0, 0.0,
+                                   0.0, 0.1, 0.0,
+                                   0.0, 0.0, 0.1 ]
+
+    imu.angular_velocity_covariance = [ -1, 0, 0,
+                                         0, 0, 0,
+                                         0, 0, 0 ]
+
+    imu.linear_acceleration_covariance = [ -1, 0, 0,
+                                            0, 0, 0,
+                                            0, 0, 0 ]
+
+    imu_pub.publish(imu)
+
+
+def main():
+    global imu_pub
+    rospy.init_node("raw_compass")
+
+    imu_pub = rospy.Publisher("compass_imu", Imu)
+
+    mag_sub = rospy.Subscriber("magnetic", Vector3Stamped, magnetic_cb)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
